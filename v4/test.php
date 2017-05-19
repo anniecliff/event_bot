c0b2988867943e5014714b180325bd11c406a13a
anniecliff <anniealencliff@gmail.com>
2017-05-19 14:45:36 +0530

    Add files via upload

--- v4/test.php
+++ v4/test.php
@@ -5,8 +5,20 @@
 //echo json_encode($data);
 header('Content-Type: application/json');
 ob_start();
-require_once ('Query.php'); 
-require 'flight/Flight.php';
+$servername = "myvoffice.me";
+$username = "myvoff_entrp";
+$password = "V2PM@.@tGr!Z";
+$database = "myvoff_vos";
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+    mysql_select_db($database);
+
 $sql = "SELECT * FROM location_info JOIN location_facilities";
 $data = getData($sql);
 $output = "hello ";
