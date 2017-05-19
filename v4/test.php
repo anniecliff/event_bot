6742602dff36b2d2e0e58b5f324be1c9b2073fce
anniecliff <anniealencliff@gmail.com>
2017-05-19 13:08:15 +0530

    Add files via upload

--- v4/test.php
+++ v4/test.php
@@ -1,9 +1,11 @@
 <?php
-require_once ('Query.php'); 
+/*require_once ('Query.php'); 
 require 'flight/Flight.php';
 
 $sql = "SELECT * FROM location_info JOIN location_facilities";
 $data = getData($sql);
-echo json_encode($data);
+echo json_encode($data);*/
+
+echo "hello";
 
 ?>
