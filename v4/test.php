17f96078bd0cee8ed3b78759b2df3da6e6087c3b
anniecliff <anniealencliff@gmail.com>
2017-05-23 09:49:05 +0530

    Add files via upload

--- v4/test.php
+++ v4/test.php
@@ -20,7 +20,7 @@ $sql = "SELECT * FROM location_info";
 $data = mysqli_query($conn,$sql);*/
 $mysqli = new mysqli("myvoffice.me", "myvoff_entrp", "V2PM@.@tGr!Z", "myvoff_vos");
 $result = $mysqli->query("SELECT * FROM location_info");
-$row = $result->fetch_assoc();
+$row = $result->fetch_array();
 //echo htmlentities($row['_message']);
 $output = "hello ";
 ob_end_clean();
