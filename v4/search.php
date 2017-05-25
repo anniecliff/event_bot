4a8058383a4f16f9a84dd0df9f82203e2af47cac
anniecliff <anniealencliff@gmail.com>
2017-05-25 11:39:17 +0530

    Add files via upload

--- v4/search.php
+++ v4/search.php
@@ -8,11 +8,21 @@ $request = json_decode($json, true);
 $action = $request["result"]["action"];
 $parameters = $request["result"]["parameters"];
 
-echo $parameters;
 
-
+/*$mysqli = new mysqli("myvoffice.me", "myvoff_entrp", "V2PM@.@tGr!Z", "myvoff_vos");
+$result = $mysqli->query("SELECT * FROM location_info WHERE location_desc like '%$search%'");
+while($row = $result->fetch_array()){
+			$data['location_name'][$i] = $row['location_desc'];
+			$i++;
+}
+*/
 
 //[Code to set $outputtext, $nextcontext, $param1, $param2 values]
+
+$next = "sendmessage-followup";
+$param1value = "1";
+$param2value = "true";
+$outputtext  = "Yes available";
 
 $output["contextOut"] = array(array("name" => "$next-context", "parameters" =>
 array("param1" => $param1value, "param2" => $param2value)));
