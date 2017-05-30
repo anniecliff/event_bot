f3476848c29448be716de77df3a86d5480a00572
anniecliff <anniealencliff@gmail.com>
2017-05-30 10:02:48 +0530

    Add files via upload

--- v4/index.php
+++ v4/index.php
@@ -77,7 +77,7 @@ Flight::route('/searchCenter', function()
 
 	ob_start();
 	
-	$obj = json_decode(file_get_contents('https://api.api.ai/api'),true);
+	$obj = json_decode(file_get_contents('php://input'),true);
 
 	$parameters = $obj['parameters'];
 	$search = $parameters['search'];
