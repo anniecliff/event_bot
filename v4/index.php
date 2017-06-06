552242d15a4364bfcc47e3860700f1eb5a5e5aad
anniecliff <anniealencliff@gmail.com>
2017-06-06 12:30:34 +0530

    Add files via upload

--- v4/index.php
+++ v4/index.php
@@ -1025,7 +1025,7 @@ function getBookingDetails()
 			   {*/
 			
 					  $myquery = "SELECT * FROM client_booking_log WHERE book_id='$bookid' AND status='1'";
-					  echo $myquery;
+					  //echo $myquery;
 			        $booking_details = getData($myquery);
 				
 				     while($data = mysqli_fetch_array($booking_details))
@@ -1055,12 +1055,12 @@ function getBookingDetails()
 						$f_time = $starth ."-".$end_time;
 						$query3 = "SELECT location_id FROM facilities_booking WHERE".$starth." = ".$data['book_id']." AND facilities_type='".$data['facilities_type']."' AND book_date='".$data['book_date']."'";
 						$res3  = getData($query3);
-						/*while($r1 = mysqli_fetch_array($res3))
+						while($r1 = mysqli_fetch_array($res3))
 						{
 								$loc_id = $r1['location_id'];
 						
-						}*/
-						echo $query3;
+						}
+						//echo $query3;
 						$loc_id=14;
 //						$loc_id = $bookingfunc->get_location_id_via_booking_table($data["facilities_type"], $data["book_date"], $starth, $data["book_id"]);
 						$vo_name = getVOName($loc_id);
