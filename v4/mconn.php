c2e79291019b293ae4b9773829c6aca9cf1db150
anniecliff <anniealencliff@gmail.com>
2017-06-15 09:13:02 +0530

    Delete mconn.php

--- v4/mconn.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include "mdb.php";
-$dbm = mysql_connect (DB_HOST, DB_USER, DB_PASSWORD) or die ('I cannot connect to the database because: ' . mysql_error());
-mysql_select_db (DB_NAME) or die("Could not select database \n");
-
-
-?>
