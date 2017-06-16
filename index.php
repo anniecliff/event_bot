1798fc774c82a385efc792ba6602bb061da30429
anniecliff <anniealencliff@gmail.com>
2017-06-16 11:33:02 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,65 @@
 <?php
+/*************** EVENT BOT APIs' **************/
 require_once ('Query.php'); 
 require 'flight/Flight.php';
 
-echo "hiiii";
+//echo "hiiii";
+//Route Function 
+//Created By Annie, June 14,2017
+Flight::route('POST /', function()
+{
+		
+		header('Content-type:application/json;charset=utf-8');
+		header('Authorization:Bearer 0ccb5842a2b04d0b9fdf23cddd01209d');
+		
+		ob_start();
+		
+		//$data = Flight::request()->data;
+		$json_obj = file_get_contents('php://input'); 
+		$request = json_decode($json_obj, true);
+		$action = $request["result"]["action"];
+		$parameters = $request["result"]["parameters"];
+		
+	
+	
+	
+});
+Flight::route('POST /searchEvents 'function(){
+
+	$returnarray=searchEvents();
+	header('Content-type:application/json;charset=utf-8');
+	echo json_encode($returnarray);
+
+
+
+});
+
+
+Flight::start();
+
+
+function searchEvents()
+{
+	$keyword = $_POST['event'];
+	echo $keyword;
+	$query = "SELECT * FROM entrp_events WHERE eventName like '%".$keyword."%'";
+	$result = getData($query);
+	if(mysqli_num_rows($result) > 0)
+	{
+		while($row = mysqli_fetch_array($result))
+		{
+				$event['eventName']		 = $row["eventName"];
+				$event["description"]	 =	$row["description"];
+				$event["address"]			 =	$row["address"];
+				$event["event_date"]		 =	$row["event_date"];
+				$event["time"]				 =	$row["event_time"];
+		}
+	}	
+	else 
+	{
+			$event = "no results found";
+	}	
+	return $event;
+}
 
 ?>
