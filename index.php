c9cb63de5f7b1aa586db724b3393c7f68898cca7
anniecliff <anniealencliff@gmail.com>
2017-06-19 10:18:43 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ function searchEvents()
 {
 	$keyword = $_POST['event'];
 	//echo $keyword;
-	$query = "SELECT * FROM entrp_events WHERE eventName like '%".$keyword."%'";
+	$query = 'SELECT * FROM entrp_events WHERE eventName like "%'.$keyword.'"%';
 	$result = getData($query);
 	if(mysqli_num_rows($result) > 0)
 	{
